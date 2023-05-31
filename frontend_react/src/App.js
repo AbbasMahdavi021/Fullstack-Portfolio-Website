34499fd736a07236e36d6265bcb15d9383ea713a
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-05-30 21:22:08 -0700

    import temporary assest, create Navbar.jsx, add SCSS files for Navbard.jsx, add more App.scss styling

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import './App.scss';
+
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
 
 
 const App = () => (
