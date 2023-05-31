5bd90f0427ad041bec5d18017b209ebc2d4b67af
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-05-31 00:23:41 -0700

    implement HigherOrderComponent (HOC) system with App Wrapper, Wrap About page in HOC, add sidebar dots slider, add Socials icons to AppWrap, which will show on all pages

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+
+const SocialMedia = () => (
+    <div className="app__social">
+        <div>
+            <BsGithub />
+        </div>
+        <div>
+            <BsLinkedin />
+        </div>
+    </div>
+);
+
+export default SocialMedia
