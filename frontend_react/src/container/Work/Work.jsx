20f5f067049d69212db84e98dc72aacf264e0bc1
AbbasMahdavi021 <abbas.mahdavi021@gmail.com>
2023-06-18 00:00:47 -0700

    Modify work  hover on smaller devices

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -71,7 +71,6 @@ const Work = () => {
                 <img src={urlFor(work.imgUrl)} alt={work.name} />
 
                 <motion.div
-                  whileHover={{ opacity: [0, 1] }}
                   transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                   className="app__work-hover app__flex"
                 >
